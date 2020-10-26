23372bab161f1334eccf4088702e79b188ae66aa
Alexander Marks <aomarks@google.com>
2020-10-26 14:07:35 -0700

    Clean up css generator

--- scripts/codemirror-css.js
+++ scripts/codemirror-css.js
@@ -3,238 +3,265 @@ import * as fs from 'fs';
 import * as path from 'path';
 import {fileURLToPath} from 'url';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const customPropertyDefinitions = [
+  {
+    name: '--playground-code-background',
+    matcher: /^\.cm-s-[^ \.]+\.CodeMirror$/,
+    cmProperty: 'background',
+  },
+  {
+    name: '--playground-code-gutter-background',
+    matcher: /^(\.cm-s-[^ \.]+)?\.CodeMirror-gutters$/,
+    cmProperty: 'background-color',
+  },
+  {
+    name: '--playground-code-activeline-background',
+    matcher: /^(\.cm-s-[^ \.]+)?\s*\.CodeMirror-activeline-background$/,
+    cmProperty: 'background',
+  },
+  {
+    name: '--playground-linenumber-color',
+    matcher: /^(\.cm-s-[^ \.]+)?\s*\.CodeMirror-linenumber$/,
+    cmProperty: 'color',
+  },
+  {
+    name: '--playground-code-color',
+    matcher: /^(\.cm-s-[^ \.]+)?\.CodeMirror$/,
+    cmProperty: 'color',
+  },
+  ...[
+    'variable',
+    'variable-2',
+    'variable-3',
+    'comment',
+    'string',
+    'string-2',
+    'keyword',
+    'type',
+    'atom',
+    'builtin',
+    'attribute',
+    'def',
+    'error',
+    'bracket',
+    'tag',
+    'header',
+    'link',
+    'number',
+    'property',
+    'meta',
+    'operator',
+    'qualifier',
+  ].map((name) => ({
+    name: `--playground-code-${name}`,
+    cmProperty: 'color',
+    matcher: new RegExp(`^.cm-s-[^ \\.]*\\s*(span)?\\.cm-${name}$`),
+  })),
+];
 
-const interestingProperties = ['color', 'background', 'background-color'];
-
-const findInterestingCategory = (selectorText) => {
-  const appliesTo = [];
-  const selectors = selectorText.split(/\s*,\s*/);
-  for (let selector of selectors) {
-    // All theme rules begin with ".cm-s-<themename>". Ignore.
-    selector = selector.replace(/^\s*\.cm-s-[a-z0-9_-]+\s*/, '');
-    if (selector === '.CodeMirror') {
-      appliesTo.push('');
-      continue;
-    }
-
-    // Many theme rules match span, not meaningful. Ignore.
-    selector = selector.replace(/^span\./, '.');
-
-    // E.g. .cm-comment, .cm-atom, .cm-keyword
-    const match1 = selector.match(/^\.cm-([a-z0-9\-]+)$/);
-    if (match1 !== null) {
-      appliesTo.push('-' + match1[1]);
-      continue;
-    }
-
-    // E.g. .CodeMirror-gutters, .CodeMirror-cursor
-    const match2 = selector.match(/^\.CodeMirror-([a-z0-9\-]+)$/);
-    if (match2 !== null) {
-      appliesTo.push('-' + match2[1]);
-      continue;
-    }
-
-    // ???
-    console.log({selector});
+function propertyForRule(rule) {
+  if (!rule.style | !rule.selectorText) {
+    return;
   }
-  return appliesTo;
+  const selectorText = rule.selectorText.trim();
+  for (const {name, cmProperty, matcher} of customPropertyDefinitions) {
+    if (selectorText.match(matcher)) {
+      const style = rule.style;
+      if (style[cmProperty]) {
+        return {name, prop: cmProperty};
+      }
+    }
+  }
+}
+
+/**
+ * Given:
+ *  foo, bar { color: blue; }
+ *
+ * Replace with:
+ *   foo { color: blue; }
+ *   bar { color: blue; }
+ */
+const denormalizeRules = (rules) => {
+  for (let r = 0; r < rules.cssRules.length; r++) {
+    const rule = rules.cssRules[r];
+    if (!rule.selectorText) {
+      continue;
+    }
+    const selectors = rule.selectorText.split(',');
+    if (selectors.length > 1) {
+      rule.selectorText = selectors[0];
+      const ruleBody = rule.cssText.replace(/.*{/, '{');
+      for (let s = 1; s < selectors.length; s++) {
+        rules.insertRule(`${selectors[s]} ${ruleBody}`, r + s);
+        r++;
+      }
+    }
+  }
 };
 
 const rewriteDefault = (rules) => {
+  console.log();
+  console.log('========================');
+  console.log('default');
+  console.log('========================');
+
+  // TODO(aomarks) We only need to do this for rules with some matching
+  // property.
+  denormalizeRules(rules);
+
+  const unhandled = new Set(customPropertyDefinitions.map((prop) => prop.name));
+  const handled = new Set();
   for (const rule of rules.cssRules) {
-    if (!rule.style | !rule.selectorText) {
+    const match = propertyForRule(rule);
+    if (match === undefined) {
       continue;
     }
-    const cats = findInterestingCategory(rule.selectorText || '');
-    if (cats.length === 0) {
-      continue;
-    }
-    rule.selectorText = rule.selectorText.replace('.cm-s-default', '');
     const style = rule.style;
-    for (let prop of interestingProperties) {
-      if (!style[prop]) {
-        continue;
-      }
-      const realProp = prop;
-      const value = style[prop];
-      if (prop === 'color') {
-        prop = '';
-      } else if (prop === 'background-color') {
-        prop = '-background';
-      } else {
-        prop = '-' + prop;
-      }
-      style[realProp] = `var(--playground-code${cats[0]}${prop}, ${value})`;
+    const current = style[match.prop];
+    style[match.prop] = `var(${match.name}, ${current})`;
+    unhandled.delete(match.name);
+    handled.add(match.name);
+    //console.log(rule.selectorText);
+    //console.log('  ', style[match.prop]);
+    //console.log();
+  }
+  if (unhandled.size > 0) {
+    console.log('UNHANDLED CUSTOM PROPS:');
+    for (const p of unhandled) {
+      console.log(`  ${p}`);
     }
   }
+  console.log('========================');
+  return rules.toString();
 };
 
-const rewriteTheme = (themeName, rules) => {
-  const result = [];
+const rewriteTheme = (rules, themeName) => {
+  console.log();
+  console.log('========================');
+  console.log(themeName);
+  console.log('========================');
+  denormalizeRules(rules);
+  const newRules = [];
   for (const rule of rules.cssRules) {
-    if (!rule.style || !rule.selectorText) {
-      continue;
-    }
-    const cats = findInterestingCategory(rule.selectorText);
-    if (cats.length == 0) {
+    const match = propertyForRule(rule);
+    if (match === undefined) {
+      console.log('UNMATCHED', rule.cssText);
       continue;
     }
     const style = rule.style;
-    for (let prop of interestingProperties) {
-      if (!style[prop]) {
-        continue;
-      }
-      const value = style[prop];
-      if (prop === 'color') {
-        prop = '';
-      } else if (prop === 'background-color') {
-        prop = '-background';
-      } else {
-        prop = '-' + prop;
-      }
-      for (const cat of cats) {
-        result.push(`--playground-code${cat}${prop}: ${value};`);
-      }
-    }
+    const current = style[match.prop];
+    const newRule = `${match.name}: ${current};`;
+    newRules.push(newRule);
+    //console.log(
+    //  `${rule.selectorText} { ${match.prop}: ${style[match.prop]}; }`
+    //);
+    //console.log('  ', newRule);
+    //console.log();
   }
-  return `.playground-theme-${themeName} {
-  ${result.join('\n  ')}
+  console.log('========================');
+  return `.theme-${themeName} {
+${newRules.sort().join('\n')}
 }`;
 };
 
-try {
-  fs.mkdirSync(path.join(__dirname, '..', '_codemirror'));
-} catch {}
-try {
-  fs.mkdirSync(path.join(__dirname, '..', '_codemirror', 'themes'));
-} catch {}
-
-try {
-  fs.mkdirSync(path.join(__dirname, '..', 'src', '_codemirror'));
-} catch {}
-try {
-  fs.mkdirSync(path.join(__dirname, '..', 'src', '_codemirror', 'themes'));
-} catch {}
-
-const codeMirrorDefaultCssPath = path.join(
-  __dirname,
-  '..',
-  'node_modules',
-  'codemirror',
-  'lib',
-  'codemirror.css'
-);
-const codeMirrorDefaultCss = fs.readFileSync(codeMirrorDefaultCssPath, 'utf8');
-const rules = cssom.parse(codeMirrorDefaultCss);
-rules.insertRule('.CodeMirror {background: inherit;}');
-rewriteDefault(rules);
-const outPath = path.join(
-  __dirname,
-  '..',
-  '_codemirror',
-  'themes',
-  'default.css'
-);
-fs.writeFileSync(outPath, rules.toString(), 'utf8');
-
-const tsOutPath = path.join(
-  __dirname,
-  '..',
-  'src',
-  '_codemirror',
-  'themes',
-  'default.css.ts'
-);
-const defaultTs = `import {css} from 'lit-element';
-const style = css\`${rules.toString()}\`;
+const cssModule = (cssText) => `import {css} from 'lit-element';
+const style = css\`${cssText}\`;
 export default style;
 `;
-fs.writeFileSync(tsOutPath, defaultTs, 'utf8');
 
-const codeMirrorThemesDir = path.join(
-  __dirname,
-  '..',
-  'node_modules',
-  'codemirror',
-  'theme'
-);
+function main() {
+  console.log('\n\n\n\n');
 
-const codeMirrorThemeFiles = fs.readdirSync(codeMirrorThemesDir);
-for (const themeFile of codeMirrorThemeFiles) {
-  const css = fs.readFileSync(
-    path.join(codeMirrorThemesDir, themeFile),
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const rootDir = path.resolve(__dirname, '..');
+  const cmDir = path.join(rootDir, 'node_modules', 'codemirror');
+  const ourThemeCssDir = path.join(rootDir, '_codemirror', 'themes');
+  const ourThemeTsDir = path.join(rootDir, 'src', '_codemirror', 'themes');
+
+  const mkdirs = [
+    ['_codemirror'],
+    ['_codemirror', 'themes'],
+    ['src', '_codemirror'],
+    ['src', '_codemirror', 'themes'],
+  ];
+  for (const dir of mkdirs) {
+    try {
+      fs.mkdirSync(path.join(__dirname, '..', ...dir));
+    } catch (e) {
+      if (e.code !== 'EEXIST') {
+        throw e;
+      }
+    }
+  }
+
+  // Theme styles
+  const themeNames = [];
+  const themeCssFilenames = fs.readdirSync(path.join(cmDir, 'theme'));
+  for (const cssFilename of themeCssFilenames) {
+    const themeName = cssFilename.replace(/\.css$/, '');
+    themeNames.push(themeName);
+    const cmThemeCss = fs.readFileSync(
+      path.join(cmDir, 'theme', cssFilename),
+      'utf8'
+    );
+    const ourThemeCss = rewriteTheme(cssom.parse(cmThemeCss), themeName);
+    // .css
+    fs.writeFileSync(
+      path.join(ourThemeCssDir, cssFilename),
+      ourThemeCss,
+      'utf8'
+    );
+    // .css.ts
+    fs.writeFileSync(
+      path.join(ourThemeTsDir, cssFilename + '.ts'),
+      cssModule(ourThemeCss),
+      'utf8'
+    );
+  }
+
+  // Default style
+  const cmDefaultCss = fs.readFileSync(
+    path.join(cmDir, 'lib', 'codemirror.css'),
     'utf8'
   );
-  const rules = cssom.parse(css);
-  const result = rewriteTheme(themeFile.replace(/\.css$/, ''), rules);
-  const outPath = path.join(
-    __dirname,
-    '..',
-    '_codemirror',
-    'themes',
-    themeFile
+  const ourDefaultCss = rewriteDefault(cssom.parse(cmDefaultCss));
+  // .css
+  fs.writeFileSync(
+    path.join(ourThemeCssDir, 'default.css'),
+    ourDefaultCss,
+    'utf8'
   );
-  fs.writeFileSync(outPath, result, 'utf8');
-
-  const tsOutPath = path.join(
-    __dirname,
-    '..',
-    'src',
-    '_codemirror',
-    'themes',
-    themeFile + '.ts'
+  // .css.ts
+  fs.writeFileSync(
+    path.join(ourThemeTsDir, 'default.css.ts'),
+    cssModule(ourDefaultCss),
+    'utf8'
   );
-  const ts = `import {css} from 'lit-element';
-const style = css\`${result}\`;
-export default style;
-`;
-  fs.writeFileSync(tsOutPath, ts, 'utf8');
-}
 
-const manifestPath = path.join(
-  __dirname,
-  '..',
-  'src',
-  '_codemirror',
-  'themes',
-  'manifest.ts'
-);
-const manifestTs = `export const themeNames = [
-${codeMirrorThemeFiles
-  .sort((a, b) => a.localeCompare(b))
-  .map((themeFile) => `  '${themeFile.replace(/\.css$/, '')}',`)
-  .join('\n')}
+  // Manifest
+  themeNames.sort();
+  const manifestTs = `export const themeNames = [
+${themeNames.map((themeName) => `  '${themeName}',`).join('\n')}
 ] as const;
 `;
-fs.writeFileSync(manifestPath, manifestTs, 'utf8');
+  fs.writeFileSync(path.join(ourThemeTsDir, 'manifest.ts'), manifestTs, 'utf8');
 
-const allThemesPath = path.join(
-  __dirname,
-  '..',
-  'src',
-  '_codemirror',
-  'themes',
-  'all.ts'
-);
-const allThemesTs = `
-${codeMirrorThemeFiles
-  .sort((a, b) => a.localeCompare(b))
+  // All themes
+  const allThemesTs = `
+${themeNames
   .map(
     (themeFile) =>
-      `import t${themeFile
-        .replace(/\.css$/, '')
-        .replace(/-/g, '_')} from './${themeFile}.js';`
+      `import t${themeFile.replace(/-/g, '_')} from './${themeFile}.css.js';`
   )
   .join('\n')}
 
 export const themeStyles = [
-${codeMirrorThemeFiles
-  .sort((a, b) => a.localeCompare(b))
-  .map(
-    (themeFile) => `  t${themeFile.replace(/\.css$/, '').replace(/-/g, '_')},`
-  )
+${themeNames
+  .map((themeFile) => `  t${themeFile.replace(/-/g, '_')},`)
   .join('\n')}
-] as const;
-`;
-fs.writeFileSync(allThemesPath, allThemesTs, 'utf8');
+] as const;`;
+  fs.writeFileSync(path.join(ourThemeTsDir, 'all.ts'), allThemesTs, 'utf8');
+}
+
+main();
